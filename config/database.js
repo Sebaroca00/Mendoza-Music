4710bf9099c6d5bd40ff66c6e8bc47f26d3b337d
Sebaroca00 <sebaroca00@gmail.com>
2024-08-15 18:07:39 -0300

    database deploy

--- config/database.js
+++ config/database.js
@@ -1,19 +1,14 @@
 const { Sequelize } = require('sequelize');
 
-// Configuración de la conexión a la base de datos
-const sequelize = new Sequelize('mendoza_music', 'postgres', 'password', {
-  host: 'localhost',
-  dialect: 'postgres',
-  logging: false, // Opcional: desactiva el logging de SQL en consola
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    port: process.env.DB_PORT || 5432,
 });
 
-// Verificación de la conexión
 sequelize.authenticate()
-  .then(() => {
-    console.log('Conexión a la base de datos establecida correctamente.');
-  })
-  .catch(err => {
-    console.error('No se pudo conectar a la base de datos:', err);
-  });
+    .then(() => console.log('Conexión a la base de datos establecida correctamente.'))
+    .catch(err => console.error('No se pudo conectar a la base de datos:', err));
 
 module.exports = sequelize;
+
